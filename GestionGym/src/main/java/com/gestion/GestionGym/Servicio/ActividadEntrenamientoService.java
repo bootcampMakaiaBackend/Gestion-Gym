0345904b5dbe6f0fb080fe4110f3415b6e255413
sant7ago <sg2518546@gmail.com>
2024-07-15 12:29:52 -0500

    [feature][task1.1][Obtener nombre mes]

--- GestionGym/src/main/java/com/gestion/GestionGym/Servicio/ActividadEntrenamientoService.java
+++ GestionGym/src/main/java/com/gestion/GestionGym/Servicio/ActividadEntrenamientoService.java
@@ -25,6 +25,23 @@ public class ActividadEntrenamientoService {
         return actividadEntrenamientoRepositorio.findAll();
     }
 
+  private String obtenerNombreMes(int mes){
+        switch (mes){
+            case 1: return "Enero";
+            case 2: return "Febrero";
+            case 3: return "Marzo";
+            case 4: return "Abril";
+            case 5: return "Mayo";
+            case 6: return "Junio";
+            case 7: return "Julio";
+            case 8: return "Agosto";
+            case 9: return "Septiembre";
+            case 10: return "Octubre";
+            case 11: return "Noviembre";
+            case 12: return "Diciembre";
+            default: return "Mes desconocido";
+        }
+  }
     //Obtener reportes mensual de un aprendiz
     // TODO: este se debe comunicar con el segundo microservicio para obtener esta informacion.
     public List<ActividadEntrenamiento> obtenerReporteMensual(Long aprendiz_id, int mes, int anio) {
