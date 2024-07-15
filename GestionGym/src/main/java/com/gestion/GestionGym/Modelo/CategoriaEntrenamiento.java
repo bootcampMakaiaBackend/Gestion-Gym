74f84e1267b737ced251b3bcc821f66be49bb618
OmarBack <omar.backend4@gmail.com>
2024-07-15 15:54:44 -0500

    [feature][task1][se creo CategoriaEntrenamiento]

--- GestionGym/src/main/java/com/gestion/GestionGym/Modelo/CategoriaEntrenamiento.java
+++ GestionGym/src/main/java/com/gestion/GestionGym/Modelo/CategoriaEntrenamiento.java
@@ -1,6 +1,38 @@
 package com.gestion.GestionGym.Modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class CategoriaEntrenamiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+
+    public CategoriaEntrenamiento() {
+    }
+
+    public CategoriaEntrenamiento(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
